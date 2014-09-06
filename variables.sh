ac8568acbbce0882d70c4645bb935b7d0d437717
Miguel Martin <miguel@miguel-martin.com>
2014-09-06 19:17:03 +0930

    fixed uninstall script, now works as expected

--- variables.sh
+++ variables.sh
@@ -1,6 +1,10 @@
-UPLOAD_SERVICE_INSTALL_PATH=~/Library/Services
-SCREENSHOT_SERVICE_INSTALL_PATH=~/Library/Workflows/Applications/Folder\ Actions
+UPLOAD_SERVICE_INSTALL_PATH=~/Library/Services/
+SCREENSHOT_SERVICE_INSTALL_PATH=~/Library/Workflows/Applications/Folder\ Actions/
 IMGURU_INSTALL_PATH=/usr/local/bin
 
-AUTOMATOR_IMGUR_SCREENSHOT_UPLOAD_DIR=./automator/imgur-screenshot-upload.workflow
-AUTOMATOR_IMGUR_UPLOAD_DIR=./automator/imgur-upload.workflow
+IMGUR_SCREENSHOT_WORKFLOW=imgur-screenshot-upload.workflow
+IMGUR_UPLOAD_WORKFLOW=imgur-upload.workflow
+
+AUTOMATOR_IMGUR_SCREENSHOT_UPLOAD_DIR=./automator/$IMGUR_SCREENSHOT_WORKFLOW
+AUTOMATOR_IMGUR_UPLOAD_DIR=./automator/$IMGUR_UPLOAD_WORKFLOW
+
