20282d9f40891c79a05060471cdb7f74f6cba00e
Gavin Aiken <gav.aiken@gmail.com>
2015-04-27 01:18:10 -0400

    Fix bugs which stopped me from installing the workflows, additionally cleanup a little to help others in future. - set -e - Script reported 'success' even when commands failed, instead fail first on commands with errors - sed delimiter from '/' to '|' - Change the delimiter used by sed so that paths are replaced correctly - which imguru - If imguru is already on the path then there's no need to copy it again

--- install.sh
+++ install.sh
@@ -11,21 +11,19 @@ AUTOMATOR_DOCUMENT_FILE=/Contents/document.wflow
 # Determine the directory in which screenshots will be saved
 if [ "x$1" != "x" ]; then
     SCREENSHOT_DIR_TO_USE=$1
-	./set_screenshot_dir.sh $SCREENSHOT_DIR_TO_USE
+    ./set_screenshot_dir.sh $SCREENSHOT_DIR_TO_USE
 elif ! SCREENSHOT_DIR_TO_USE=$(defaults read com.apple.screencapture location); then
-	# Screenshot dir is OS default by omission of any preference to the contrary
-    echo "screenshot dir default: \"$HOME/Desktop\""
+    # Screenshot dir is OS default by omission of any preference to the contrary
+    SCREENSHOT_DIR_TO_USE=~/Desktop
 fi
 
-if [ -z ${SCREENSHOT_DIR_TO_USE+x} ]; then
-    echo "screenshot dir is: \"$SCREENSHOT_DIR_TO_USE\""
-fi
+echo "screenshot dir is: \"$SCREENSHOT_DIR_TO_USE\""
 
 # copy the binary to upload images if it's not already on the PATH
 if IMGURU_EXISTING_PATH=$(which imguru); then
-	echo "existing imguru found $IMGURU_EXISTING_PATH"
+    echo "existing imguru found: $IMGURU_EXISTING_PATH"
 else
-    echo "copying imguru to $IMGURU_INSTALL_PATH"
+    echo "copying imguru to: $IMGURU_INSTALL_PATH"
     cp ./imgur-uploader/imguru $IMGURU_INSTALL_PATH
 fi
 
@@ -35,13 +33,12 @@ echo "installing..."
 cp -r $AUTOMATOR_IMGUR_SCREENSHOT_UPLOAD_TEMPLATE_DIR $AUTOMATOR_IMGUR_SCREENSHOT_UPLOAD_DIR
 cp -r $AUTOMATOR_IMGUR_UPLOAD_TEMPLATE_DIR $AUTOMATOR_IMGUR_UPLOAD_DIR
 
-# alter the copied screenshot automator template to set the screenshot dir
-sed -i '' 's/{SCREENSHOT_DIR}/${SCREENSHOT_DIR_TO_USE}/g' $AUTOMATOR_IMGUR_SCREENSHOT_UPLOAD_DIR$AUTOMATOR_DOCUMENT_FILE
+# replace variable placeholder for the path to the shell executable
+sed -i '' "s|{SHELL}|${SHELL}|g" $AUTOMATOR_IMGUR_SCREENSHOT_UPLOAD_DIR$AUTOMATOR_DOCUMENT_FILE
+sed -i '' "s|{SHELL}|${SHELL}|g" $AUTOMATOR_IMGUR_UPLOAD_DIR$AUTOMATOR_DOCUMENT_FILE
 
-SHELL_PATH=$(echo $SHELL | sed 's/\//\\\//g')
-# set the appropriate shells for both automator scripts
-sed -i '' "s/{SHELL}/${SHELL_PATH}/g" $AUTOMATOR_IMGUR_SCREENSHOT_UPLOAD_DIR$AUTOMATOR_DOCUMENT_FILE
-sed -i '' "s/{SHELL}/${SHELL_PATH}/g" $AUTOMATOR_IMGUR_UPLOAD_DIR$AUTOMATOR_DOCUMENT_FILE
+# replace variable placeholder for the screenshot directory
+sed -i '' "s|{SCREENSHOT_DIR}|${SCREENSHOT_DIR_TO_USE}|g" $AUTOMATOR_IMGUR_SCREENSHOT_UPLOAD_DIR$AUTOMATOR_DOCUMENT_FILE
 
 # now install the automator files
 mkdir -p "$SCREENSHOT_SERVICE_INSTALL_PATH"
